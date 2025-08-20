5f663ad3c4b2adfd8a47dbffcb0eaa2a03705421
nqtienvn <quangtiencoder1876@gmail.com>
2025-08-20 14:43:51 +0700

    first commit create Spring boot project

---
+++ src/main/java/com/codemaster/tien/CodeMasterApplication.java
@@ -0,0 +1,13 @@
+package com.codemaster.tien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codemaster/tien/CodeMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemaster.tien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
